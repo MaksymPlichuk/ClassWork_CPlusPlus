2cb15b26f5c1c96f08653b5a0411fef9ec943fed
PlXch_dv66@student.itstep.org <PlXch_dv66@student.itstep.org>
2025-03-03 17:52:19 +0000

    Task 1-3

---
+++ 05_for/05_for.cpp
@@ -0,0 +1,101 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//Завдання 1. Вивести на екран усі числа від нуля до вве -
+//деного користувачем числа.
+//
+//Завдання 2. Користувач вводить дві межі діапазону, ви -
+//вести на екран усі числа з цього діапазону.Передбачити,
+//
+//щоб користувач міг вводити межі діапазону в довільному
+//порядку.
+//■ вивести всі парні числа з діапазону.
+//■ вивести всі непарні числа з діапазону.
+//■ вивести всі числа, кратні семи.
+//
+
+//
+//Завдання 4. Користувач з клавіатури вводить числа.По -
+//рахувати їхню суму і вивести на екран, щойно користувач
+//
+//введе нуль.
+int main()
+{
+	/*cout << "Task1" << endl;
+	int num;
+	cout << "To which number enter";
+	cin >> num;
+		for (int i = 0; i <= num; i++)
+	{
+			cout << i << " ";
+	}*/
+
+	/*cout << "Task2" << endl;
+	int num1;int num2;
+	cout << "Enter range"<< endl;
+	cin >> num1; cin >> num2;
+	int start = num1;
+	int end = num2;
+	if (num1 > num2)
+	{
+		swap(num1, num2);
+	}
+	cout << "All numbers form the range" << "  ";
+	for (num1; num1<=num2; num1++)
+	{
+			cout << num1 << " ";
+	}
+	cout << endl;
+	num1 = start; num2 = end;
+	cout << "Even numbers" << "  ";
+	for (num1; num1<=num2; num1++)
+	{
+		if (num1 % 2 == 0)
+			cout << num1 << " ";
+	}
+	cout << endl;
+	num1 = start; num2 = end;
+	cout << "Numbers Uneven" << "  ";
+	for (num1; num1 <= num2; num1++)
+	{
+		if (num1 % 2 != 0)
+			cout << num1 << " ";
+	}
+	cout << endl;
+	num1 = start; num2 = end;
+	cout << "Numbers Even 7" << "  ";
+	for (num1; num1 <= num2; num1++)
+	{
+		if (num1 % 7 == 0)
+			cout << num1 << " ";
+	}
+	cout << endl;*/
+
+	//Завдання 3. Користувач вводить дві межі діапазону.По -
+//рахувати суму всіх чисел діапазону.
+	/*cout << "Task3" << endl;
+	int start1;
+	int end1;
+	cout << "Enter Start";
+	cin >> start1;
+	cout << "Enter END";
+	cin >> end1;
+	int sum = 0;
+	if (start1 > end1)
+	{
+		swap(start1, end1);
+	}
+	for (start1; start1 <= end1; start1++)
+	{
+		cout << start1 << " ";
+		sum += start1;
+	}
+	cout << "Summa = ";
+	cout << sum;*/
+
+	/*cout << "Task4" << endl;
+	for (;;)*/
+
+}
+
