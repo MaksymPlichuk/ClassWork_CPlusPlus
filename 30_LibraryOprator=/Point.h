30e71cebba4b43fc0b1f7a657b82b17a62331a6f
dev <dev@A21PC6>
2025-04-30 17:55:13 +0300

    Task 1 to/do

---
+++ 30_LibraryOprator=/Point.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+using namespace std;
+/*Тема: Move Constructor. operator =
+//Завдання 1:
+//Створити клас Point – точка на площині (x, y). Реалізувати набір відповідних констуркторів, метод Print() та перевантажити наступні оператори:
+//-- ++ – збільшує/зменшує кожну координату на 1
+//> < – перевіряє точки на розташування (ближче/далі) відносно початкової координати (0:0)
+//! – змінює значення координат на протилежне
+//Class Point
+//{
+//int x;
+//int y;
+//}*/
+class Point
+{
+	int x;
+	int y;
+public:
+	Point() :x(0), y(0) {}
+	Point(int x, int y) :x(x), y(y) {}
+	
+	void print()const;
+	Point operator--();
+	Point operator++();
+	Point operator>();
+	Point operator<();
+	
+};
+
